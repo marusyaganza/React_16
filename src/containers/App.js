28838784ae2c7685d0a5f9273f22cc0660d264a5
marusyaganza <marusyaganza@yandex.ru>
2018-01-29 22:24:32 +0300

    Up to lesson 68

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import styles from '../assets/App.css';
-import Person from '../components/Persons/Person/Person';
-import Errorboundary from './ErrorBoundary/ErrorBoundary'
 import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
 
@@ -41,23 +39,23 @@ class App extends Component {
                 persons: persons
             }
         )
-    }
+    };
     togglerPersonsHandler = () => {
         const doesShow = this.state.showPersons;
         this.setState({showPersons: !doesShow});
-    }
+    };
     deletePersonHandler = (personIndex) => {
         const persons = [...this.state.persons];
         persons.splice(personIndex, 1);
         this.setState({persons: persons})
-    }
+    };
 
     render() {
         let persons = null;
 
         if (this.state.showPersons) {
             persons = (
-                persons = <Persons
+                <Persons
                     persons={this.state.persons}
                     clicked={this.deletePersonHandler}
                     changed={this.nameChangedHandler}/>
